b2b52ae289d6854fa2167a7cab69f0879e083a12
Yaryna <ykurdoba@gmail.com>
2019-10-09 19:31:01 -0400

    Created Movie UI Created Movie Class

---
+++ Week03/MovieApplication/MovieApplication/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApplication
+{
+    class Movie
+    {
+        public string Title;
+        public List<string> Actors;
+        public string Genre;
+        public double ReviewScore;
+
+        public void Watch()
+        {
+
+        }
+    }
+}
